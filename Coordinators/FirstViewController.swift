d9e4dbd77ee37be5ac9c67be1ea4647e08fdc377
Dimitar Chakarov <dimitar@dchakarov.com>
2019-11-06 21:30:00 +0000

    Initial Commit

---
+++ Coordinators/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Coordinators
+//
+//  Created by Dimitar Chakarov on 06/11/2019.
+//  Copyright © 2019 Dimitar Chakarov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coordinators/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Coordinators
+//
+//  Created by Dimitar Chakarov on 06/11/2019.
+//  Copyright © 2019 Dimitar Chakarov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
